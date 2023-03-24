d3ce4faae92e16f798c0ae701ab8fc198c03fbfc
Osokin.Art <aptem21@mail.ru>
2023-03-24 17:31:37 +0400

    fix addLogo for folders and polls

--- backend/src/graphql_utils/add_logos.py
+++ backend/src/graphql_utils/add_logos.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_file_upload.scalars import Upload
 
 from common.utils import change_disk_space, get_model
+from folders.models import Folder
 from graphql_utils.utils_graphql import (
     check_disk_space,
     download_logo,
@@ -32,7 +33,10 @@ class UploadLogo(graphene.Mutation):
                response=None, **kwargs):
         PermissionClassFolder.has_permission(info)
         instance = get_model(model).objects.filter(pk=document_id).first()
-        host_folder = instance.folder
+        if isinstance(instance, Folder):
+            host_folder = instance
+        else:
+            host_folder = instance.folder
 
         PermissionClassFolder.has_mutate_object_permission(info, host_folder.id)
 
