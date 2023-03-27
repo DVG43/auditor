50309d440854814cdf6438f6f951d94913d02087
Sergey Kovalev <svkoval@mail.ru>
2023-03-28 02:03:54 +0900

    text to speech

--- backend/src/document/urls.py
+++ backend/src/document/urls.py
@@ -11,19 +11,12 @@ from document.views import (
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('projects/document/text_rephrase/', TextRephrase.as_view()),
-    path('projects/document/text_generator/', TextGeneration.as_view()),
-    path('projects/document/text_shorter/', TextShorter.as_view()),
-    path('projects/document/text_continue/', TextContinue.as_view()),
-    path('projects/document/image_generator/', ImageGeneration.as_view()),
-    path('projects/document/text2speech/<int:pk>/', Text2Speech.as_view()),
-=======
->>>>>>> develop
     path('folders/document/<int:pk>/', DicumentViewSet.as_view({'get': 'retrieve'})),
     path('folders/<int:pk>/document/', ProjectViewSet.as_view({'get': 'retrieve'})),
     path('projects/document-logo/<int:doc_pk>/', ChangeDocumentLogoView.as_view()),
 
     ## Image generation
     path('projects/document/image_generator/', ImageGeneration.as_view()),
+    ## Text to speech
+    path('projects/document/text2speech/<int:pk>/', Text2Speech.as_view()),
 ]
