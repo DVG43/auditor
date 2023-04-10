64bd16365a73e1e9405a0ae1979e4d619577f6c5
d-01 <any@mail.com>
2023-04-10 18:05:42 +0300

    Тестовая страница для проверки потоковой передачи (SSE)

--- backend/src/ai/urls.py
+++ backend/src/ai/urls.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+from django.http import HttpResponse
 from django.urls import path
 from . import views
 
+TEST_PAGE_HTML = Path('./ai/test_page.html').read_text()
+
+def get_test_page(request):
+    return HttpResponse(TEST_PAGE_HTML)
 
 urlpatterns = [
     # path('ai/theme_to_text/', views.ThemeToText.as_view()),
@@ -9,4 +15,6 @@ urlpatterns = [
     # path('ai/text_continue/', views.TextContinue.as_view()),
     path('ai/query_ai/', views.QueryAi.as_view()),
     path('ai/standard_generation/', views.StandardGeneration.as_view()),
+    path('stream_test/', views.StreamTest.as_view()),
+    path('test_page.html', get_test_page),
 ]
