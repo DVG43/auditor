c1f893c734fe3ebafdcaea577eb696fed30ed5c6
Aleksei Semenov <7.on.off@bk.ru>
2023-04-13 23:45:50 +0300

    fix static

---
+++ docker/backend/source-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+$counter
+until cd /app/backend/src
+do
+    echo "Waiting for volume to be ready..."
+    if [ "$counter" -gt 3 ];
+    then
+        echo "Exiting loop!"
+
+        exit 1
+    else
+        counter=$((counter+1))
+        sleep 1
+    fi
+done
+mkdir -p share
+mkdir -p share/auditor-v2_media share/auditor-v2_media/images share/auditor-v2_media/video share/auditor-v2_media/audio share/auditor-v2_statics
+chmod -R 777 share
+touch "${APP_API_LOG}"
