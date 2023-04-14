eafa70d1e62970eda45e59190065689a67a61a10
Lev Semenov <sem.lev.a@yandex.ru>
2023-04-14 23:33:52 +0300

    fixed wrong order_id on question re-creation

--- backend/src/poll/service.py
+++ backend/src/poll/service.py
@@ -69,19 +69,22 @@ def update_order_free_answer_items(item_question_id: int, old_order_id: int, ord
 
 
 def update_questions_order(question, created=True):
-    poll = question.poll
-    questions = poll.get_questions(parent_id=question.parent_id)
-    if created:
-        question.order_id = max(questions, key=lambda x: x.order_id).order_id + 1
-        question.save()
+    if created and question.order_id:
+        return
     else:
-        questions.remove(question)
-        questions = sorted(questions, key=lambda x: x.order_id)
-        orders = [x.order_id for x in questions]
-        try:
-            position = orders.index(question.order_id)
-            for q in questions[position:]:
-                q.order_id += 1
-                q.save()
-        except ValueError:
-            return
+        poll = question.poll
+        questions = poll.get_questions(parent_id=question.parent_id)
+        if created:
+            question.order_id = max(questions, key=lambda x: x.order_id).order_id + 1
+            question.save()
+        else:
+            questions.remove(question)
+            questions = sorted(questions, key=lambda x: x.order_id)
+            orders = [x.order_id for x in questions]
+            try:
+                position = orders.index(question.order_id)
+                for q in questions[position:]:
+                    q.order_id += 1
+                    q.save()
+            except ValueError:
+                return
