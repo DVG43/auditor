c935474733ae9f77ea2294aa223a8a041a2847ab
expmax <expmax85@gmail.com>
2023-04-16 13:51:29 +0500

    add testform_by_id query

--- backend/src/testform/schema/queries.py
+++ backend/src/testform/schema/queries.py
@@ -7,8 +7,18 @@ from testform.models import TestFormQuestion, TFQuestionType
 
 
 class QueryTestForm(graphene.ObjectType):
+    testform_by_id = graphene.Field(types.TestFormType, id=graphene.ID())
     all_testform_questions = graphene.List(types.TestFormQuestionType, testform_id=graphene.Int())
     testform_question_by_id = graphene.Field(types.TestFormQuestionType, question_id=graphene.Int())
+
+    @login_required
+    def resolve_testform_by_id(self, info, id=None):
+        """
+        Получение всех вопросов по тесту
+        """
+        PermissionClass.has_permission(info)
+        ret = TestForm.objects.get(pk=id)
+        return ret
 
     @login_required
     def resolve_all_testform_questions(self, info, testform_id=None):
