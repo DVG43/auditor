a20eae5d4ab66014d29d074e385fff7ed44f45f4
Julia <bykanova.yuliya@wildberries.work>
2023-04-26 17:44:17 +0300

    add_change_color

--- backend/src/poll/static/poll/js/main.js
+++ backend/src/poll/static/poll/js/main.js
@@ -81,9 +81,9 @@ document.addEventListener("DOMContentLoaded", () => {
       answersBtn.addEventListener('click', (event) => {
         for(let i=0; i<answersBtns.length; i++){
           if(event.currentTarget === answersBtns[i]){
-            answersBtns[i].classList.add('question-answers-list__item-btn-active');
+            answersBtns[i].style.background = answersBtns[i].attributes.fill.value;
           } else{
-            answersBtns[i].classList.remove('question-answers-list__item-btn-active');
+            answersBtns[i].style.background = '#40444b'; 
           };
         };
         accordionTextGenerator(5, 5, 5);
